b9d7f404cc55c35597122e086d32f6a522023973
zhaokt19 <892198296@qq.com>
2019-11-08 15:42:25 +0800

    Revert "Initial commit"
    This reverts commit 95eea97463130a1db48eff6491e4909131ae4fd4.

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-import keras as ks
-
-while(1):
-    y = input()
-    x = eval(input())
-    print(type(y),type(x))
-    print(x**2)
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+# reading in an image
+image = mpimg.imread('whiteCarLaneSwitch.jpg')
+# printing out some stats and plotting the image
+print('This image is:', type(image), 'with dimensions:', image.shape)
+plt.imshow(image)
+plt.show()
